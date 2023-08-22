9758885d4b2d43c0557b41831dc302229be321ff
Gustavo Antonio <gustavo-antonio5@hotmail.com>
2023-08-22 18:23:26 -0300

    Finalizando desafio da calculadora, funcoes

--- src/script.js
+++ src/script.js
@@ -1,7 +1,40 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+let result = 0
+function Calculadora(number1, number2, operator) {
+  
+  number1 = Number(number1)
+  number2 = Number(number2)
+
+  switch(operator){
+    case "+":
+      result = number1 + number2
+      break;
+    case "-":
+      result = number1 - number2
+      break;
+    case "*":
+      result = number1 * number2
+      break;
+    case "/":
+      result = number1 / number2
+      break;
+    case "e":
+      result = number1 * number1
+      for (let i = 1 ; i < number2 - 1; i++) {
+        result = result * number1 
+      }
+        break;    
+  }
+
+  if(result == undefined || result > 1000000)
+    result = "ERRO"
+
+  return result;
+}
+
+Calculadora()
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
